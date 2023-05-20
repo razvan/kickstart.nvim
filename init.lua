13d31e5fadde0b95c3773b060adf8652cc6f0408
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-05-20 19:12:23 +0200

    Add keymap for Git

--- init.lua
+++ init.lua
@@ -500,5 +500,7 @@ cmp.setup {
   },
 }
 
+vim.keymap.set('n', '<leader>gg', ':Git<CR>', { desc = "Git status" })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
